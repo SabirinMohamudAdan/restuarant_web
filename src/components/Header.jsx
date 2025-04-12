29029a87764f9a62f5d7ced0407417801b714a33
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-04-11 23:35:25 -0700

    u

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -22,10 +22,23 @@ function Header() {
           <div className="flex items-center ">
             <h1 href="/" className="font-bold text-white text-4xl ">
               DELI <span className='text-[#da9360]'>Z</span>US
-              <p className='text-[15px]  ml-6'> <span className='text-gray-500 
-               mr-1 text-4xl '>- </span>
-                Resto & cafe <span className='text-gray-500 
-               mr-1 text-4xl '>- </span></p>
+              {/* <p className="text-[15px] ml-6">
+  <span className="text-gray-500 mr-1 text-4xl">-</span>
+  Resto & cafe
+  <span className="text-gray-500 mr-1 text-4xl">-</span>
+</p>
+
+*/}
+
+<p className="text-[15px] ml-2 flex items-center gap-2">
+  <svg className="w-5 h-5 text-gray-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+  </svg>
+  Resto & cafe
+  <svg className="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20">
+    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM7 9a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0z" clipRule="evenodd" />
+  </svg>
+</p>
             </h1>
           
             
