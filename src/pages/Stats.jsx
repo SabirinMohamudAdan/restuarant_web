8a336a4c351b683b27710121d3db3bb2c0a0b40b
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-04-13 10:09:12 -0700

    new

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -10,7 +10,7 @@ const StatsSection = () => {
   ];
 
   return (
-    <div className="py-16 px-4 sm:px-6 lg:px-8 bg">
+    <div className="py-16 px-4 sm:px-6 lg:px-8 bg-amber-500">
       <div className="max-w-7xl mx-auto">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
           {stats.map((stat, index) => (
