6a39fb242a3f9ca7e23948a0e656e74f1b0cd108
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-04-14 13:44:08 -0700

    m--

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import Home from "./pages/Home"
-
-
+// import Home from "./pages/Home"
+// import {   Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import MenuSection from './pages/Menus';
+import EventsSection from './pages/Events';
+import Header from './components/Header';
+// import Header from './Header'; // Header-kaaga
+// import Home from './pages/Home';
+// import Menu from './pages/Menu';
+// import About from './pages/About';
+// import Book from './pages/Book';
+// import Event from './pages/Event';
+// import Blog from './pages/Blog';
+// import Contact from './pages/Contact';
 function App() {
   
 
   return (
     <>
-     <Home/>
+     {/* <Home/> */}
+<Router>
+     <Header/> {/* Header-ka oo isticmaalaya Link components */}
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<MenuSection />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/book" element={<Book />} /> */}
+        <Route path="/event" element={<EventsSection />} />
+        {/* <Route path="/blog" element={<Blog />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+        {/* <Route path="*" element={<NotFound />} />  */}
+      </Routes>
+    </Router>
        
     </>
   )
