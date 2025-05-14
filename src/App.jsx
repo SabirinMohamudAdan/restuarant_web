2ac72a7a999c06ec4e3162d83556e3b70065ec9f
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-05-14 01:01:25 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -16,9 +16,41 @@
 
 
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import {  Router, Routes, Route } from 'react-router-dom';
+// import Home from "./pages/Home";
+// // import Header from "."; // Assuming your Header component is in Header.js
+// import Menu from "./pages/Menu";
+// import About from "./pages/About";
+// import Book from "./pages/Book";
+// import Event from "./pages/Event";
+// import Blog from "./pages/Blog";
+// import Contact from "./pages/Contact";
+// import Header from './components/Header';
+// import MenuSection from './pages/Menus';
+
+// function App() {
+//   return (
+//     <Router>
+//       {/* <Header /> */}
+//       <Header/>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/menu" element={<MenuSection/>} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/book" element={<Book />} />
+//         <Route path="/event" element={<Event />} />
+//         <Route path="/blog" element={<Blog />} />
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+import { Routes, Route } from 'react-router-dom';
 import Home from "./pages/Home";
-// import Header from "."; // Assuming your Header component is in Header.js
 import Menu from "./pages/Menu";
 import About from "./pages/About";
 import Book from "./pages/Book";
@@ -30,19 +62,18 @@ import MenuSection from './pages/Menus';
 
 function App() {
   return (
-    <Router>
-      {/* <Header /> */}
+    <>
       <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<MenuSection/>} />
+        <Route path="/menu" element={<MenuSection />} />
         <Route path="/about" element={<About />} />
         <Route path="/book" element={<Book />} />
         <Route path="/event" element={<Event />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
