49da5e461ca69fd8b3add9888b5545511e4347ef
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-05-14 00:19:39 -0700

    new---

--- src/App.jsx
+++ src/App.jsx
@@ -13,3 +13,34 @@ function App() {
 }
 
 // export default App
+
+
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Header from "./Header"; // Assuming your Header component is in Header.js
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Book from "./pages/Book";
+import Event from "./pages/Event";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/book" element={<Book />} />
+        <Route path="/event" element={<Event />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
