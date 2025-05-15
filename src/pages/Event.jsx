70db421e5cb90db8c3bfc1071c41b70d8c0e039b
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-05-15 11:35:14 -0700

    ..

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -760,7 +760,7 @@ const EventsSection = () => {
           ))}
         </div>
       </div>
-      <div>
+      <div className='mt-12 sm:mt-16 md:mt-20'>
         <Footer/>
       </div>
     </div>
